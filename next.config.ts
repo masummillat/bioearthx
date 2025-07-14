08d2f7cec43ce1b593d6fca1e3bbfbe51de45d27
Masum Millat <masummillat@gmail.com>
2025-07-14 18:18:12 +0600

    updated script and next config

--- next.config.ts
+++ next.config.ts
@@ -1,6 +1,9 @@
 import type { NextConfig } from "next";
 
 const repoName = "bioearthx";
+console.log("ENV:", process.env.NODE_ENV);
+console.log("basePath:", process.env.NODE_ENV === "production" ? `/${repoName}` : "");
+console.log("assetPrefix:", process.env.NODE_ENV === "production" ? `/${repoName}/` : "");
 
 const nextConfig: NextConfig = {
   output: "export",
